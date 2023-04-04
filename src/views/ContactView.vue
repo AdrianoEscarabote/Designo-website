3f1d855b49bfca46cac9417a0d648d9886dc28a1
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-04-04 11:47:29 -0300

    view design ready

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,6 @@
 <script>
 import Layout from '../components/Layout.vue';
+import ListCountries from '../components/shared/ListCountries.vue';
 
 export default {
   name: "Designo Contact",
@@ -7,7 +8,8 @@ export default {
     document.title = "Designo - Contact"
   },
   components: {
-    Layout
+    Layout,
+    ListCountries,
   }
 }
 </script>
@@ -15,7 +17,164 @@ export default {
 <template>
   <Layout>
 
-    <h1>contact</h1>
+    <main>
+      <div class="container">
+        <section class="form">
+          <div class="col1">
+            <h1>Contact Us</h1>
+            <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.</p>
+          </div>
+          <div class="col2">
+            <form novalidate>
+              <fieldset>
+                <legend className="sr-only">Enter your information in all fields</legend>
+
+                <label for="name">
+                  <input type="text" name="name" id="name" placeholder="Name" />
+                </label>
+
+                <label for="email">
+                  <input type="email" name="email" id="email" placeholder="Email Address" />
+                </label>
+
+                <label for="phone">
+                  <input type="tel" name="phone" id="phone" placeholder="Phone" />
+                </label>
+
+                <label for="message">
+                  <input type="text" name="message" id="message" placeholder="Your Message" />
+                </label>
+
+                <button type="submit">submit</button>
+
+              </fieldset>
+            </form>
+          </div>
+        </section>
+      </div>
+      <ListCountries />
+    </main>
 
   </Layout>
 </template>
+
+<style scoped>
+.container {
+  padding: 0px 165px;
+}
+
+.form {
+  background: #E7816B url("../assets/contact/desktop/bg-pattern-hero-desktop.svg") no-repeat;
+  border-radius: 15px;
+  background-position: bottom left;
+  color: #fff;
+  max-width: 1110px;
+  margin: 0 auto;
+  height: 480px;
+  padding: 0px 95px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.col1 {
+  max-width: 445px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  justify-content: center;
+}
+
+.col1 h1 {
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 48px;
+}
+
+.col1 p {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+}
+
+.col2 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 380px;
+  width: 100%;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  max-width: 380px;
+}
+
+fieldset {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border: unset !important;
+  gap: 24px;
+}
+
+input,
+label {
+  width: 100% !important;
+}
+
+input {
+  outline: transparent;
+  border: unset;
+  background: transparent;
+  border-bottom: 1px solid #FFFFFF;
+  color: #fff;
+}
+
+input::placeholder {
+  color: rgba(255, 255, 255, 0.500);
+}
+
+input::-moz-placeholder {
+  color: rgba(255, 255, 255, 0.500);
+}
+
+input::-webkit-input-placeholder {
+  color: rgba(255, 255, 255, 0.500);
+}
+
+input#name,
+input#email,
+input#phone {
+  height: 38px;
+}
+
+input#message {
+  height: 102px;
+}
+
+button {
+  align-self: flex-end;
+  width: 152px;
+  height: 56px;
+  background: #FFFFFF;
+  border-radius: 8px;
+  border: transparent;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: #333136;
+}
+
+@media (max-width: 1140px) {
+  .container {
+    padding: 0px 20px;
+  }
+}
+</style>
--- src/views/LocationsView.vue
+++ src/views/LocationsView.vue
@@ -1,5 +1,6 @@
 <script>
 import Layout from '../components/Layout.vue';
+import GetInTouch from '../components/shared/GetInTouch.vue';
 
 export default {
   name: "Designo Locations",
@@ -7,15 +8,143 @@ export default {
     document.title = "Designo - Locations"
   },
   components: {
-    Layout
-  }
+    Layout,
+    GetInTouch
+}
 }
 </script>
 
 <template>
   <Layout>
 
-    <h1>Locations</h1>
+    <main>
+      <h1 class="sr-only">See our locations below</h1>      
+      <div class="container">
+        <section class="country">
+          <div class="col1">
+            <h2>Canada</h2>
+            <div class="text">
+              <p>
+                Designo Central Office
+                <span>3886 Wellington Street</span>
+                <span>Toronto, Ontario M9C 3J5</span>
+              </p>
+              <p>
+                Contact
+                <span>P : +1 253-863-8967</span>
+                <span>M : contact@designo.co</span>
+              </p>
+            </div>
+          </div>
+          <div class="col2">
+            <img src="../assets/locations/desktop/image-map-canada.png" alt="" width="350" height="326" />
+          </div>
+        </section>
+        <section class="country">
+          <div class="col2">
+            <img src="../assets/locations/desktop/image-map-australia.png" alt="" width="350" height="326" />
+          </div>
+          <div class="col1">
+            <h2>Australia</h2>
+            <div class="text">
+              <p>
+                Designo AU Office
+                <span>19 Balonne Street</span>
+                <span>New South Wales 2442</span>
+              </p>
+              <p>
+                Contact
+                <span>P : (02) 6720 9092</span>
+                <span>M : contact@designo.au</span>
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="country">
+          <div class="col1">
+            <h2>United Kingdom</h2>
+            <div class="text">
+              <p>
+                Designo UK Office
+                <span>13 Colorado Way</span>
+                <span>Rhyd-y-fro SA8 9GA</span>
+              </p>
+              <p>
+                Contact
+                <span>P : 078 3115 1400</span>
+                <span>M : contact@designo.uk</span>
+              </p>
+            </div>
+          </div>
+          <div class="col2">
+            <img src="../assets/locations/desktop/image-map-united-kingdom.png" alt="" width="350" height="326" />
+          </div>
+        </section>
+      </div>
+      <GetInTouch />
+
+    </main>
     
   </Layout>
 </template>
+
+<style scoped>
+.container {
+  padding: 0px 165px 160px 165px;
+}
+
+.country {
+  display: flex;
+  gap: 30px;
+  height: 326px;
+  max-width: 1110px;
+  margin: 0 auto 30px auto;
+}
+
+.col1 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+  padding: 0px 95px;
+  background: #FDF3F0 url("../assets/shared/desktop/bg-pattern-two-circles.svg") no-repeat;
+  background-position: bottom left;
+  border-radius: 15px;
+  width: 100%;
+  max-width: 730px;
+}
+
+.col2 {
+  border-radius: 15px;
+  overflow: hidden;
+}
+
+h2 {
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 48px;
+  color: #E7816B;
+}
+.text {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+
+.text p {
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 26px;
+  color: #333136;
+  display: flex;
+  flex-direction: column;
+}
+
+.text span {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: #333136;
+}
+
+</style>
