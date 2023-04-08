9335a323c12437346fd2c3a6af69caa220887735
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-04-08 11:49:22 -0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Layout from "../components/Layout.vue"
+import Layout from "../components/Layout/Layout.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),  
   routes: [
     {
       path: '/',
@@ -41,6 +41,10 @@ const router = createRouter({
     }
   ]
 })
+
+router.afterEach((to, from) => {
+  window.scrollTo(0, 0);
+});
 
 router.beforeEach((to, from, next) => {
   if (to.meta.layout === undefined) {
